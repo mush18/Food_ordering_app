bba94f51896cb8c974f756eed81ff320ae06a334
Musharraf Dhalwala <dhalwalamusharraf1210@gmail.com>
2023-10-21 08:59:18 +0530

    Used Api

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,10 +1,25 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockdata";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body =()=>
 {
   const [listOfRestaurants,setListOfRestaurant]=useState(resList);
+
+  useEffect(()=>
+  {
+    fetchData()
+  },[]);
+
+
+  const fetchData=async ()=>
+  {
+    const data=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.295754&lng=73.2178655&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+    const json=await data.json();
+
+    console.log(json);
+  }
 
     return(
         <div className="body">
